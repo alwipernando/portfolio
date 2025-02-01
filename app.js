db32bca8c93597cdd6c99ac0a8261b9d2b103457
Alwi Pernando <153611504+alwipernando@users.noreply.github.com>
2025-02-01 08:15:28 +0700

    Update app.js

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 // ====typed js====
 const typed = new Typed('.multiple-text', {
-    strings: ['Junior Web Developer', 'Maha Siswa'],
+    strings: ['Web Developer', 'Maha Siswa'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
